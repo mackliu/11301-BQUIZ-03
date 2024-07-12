0862135a8bce772fbfad161894060d58f9947896
mackliu <macklun@gmail.com>
2024-07-12 13:51:27 +0800

    將預告片海報顯示在海報清單區

--- backend/poster.php
+++ backend/poster.php
@@ -1,3 +1,23 @@
+<style>
+    .row {
+        background-color: white;
+        height: 80px;
+        padding: 2px;
+        display: flex;
+        align-items: center;
+        margin: 2px 0;
+    }
+
+    .row>div {
+        text-align: center;
+        width: 25%;
+        color: black;
+    }
+
+    .row img {
+        height: 80px;
+    }
+</style>
 <div style='height:280px;'>
     <h4 class='ct'>預告片清單</h4>
     <form action="./api/edit_poster.php" method="post">
@@ -8,10 +28,35 @@
             <div style="width:24.5%;padding:3px;text-align:center;background:#ccc;color:black">操作</div>
         </div>
         <div style="height:180px;overflow:auto">
-            <div style="width:100%;height:60px;background:white">
-                
-            </div>
-
+            <?php
+            $rows = $Poster->all(" order by rank");
+            foreach ($rows as $row) {
+            ?>
+                <div class="row">
+                    <div>
+                        <img src="./images/<?= $row['img']; ?>" alt="">
+                    </div>
+                    <div>
+                        <input type="text" name="name" value="<?= $row['name']; ?>">
+                    </div>
+                    <div>
+                        <button type="button">往上</button>
+                        <button type="button">往下</button>
+                    </div>
+                    <div>
+                        <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>>顯示
+                        <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">刪除
+                        <select name="ani[]">
+                            <option value="1">淡入淡出</option>
+                            <option value="2">縮放</option>
+                            <option value="3">滑入滑出</option>
+                        </select>
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
         </div>
         <div class='ct'>
             <input type="submit" value="編輯確定">
