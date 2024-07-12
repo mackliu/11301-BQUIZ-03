f75ae2d2449dd978e2835d2c341a94b2aca9e691
mackliu <macklun@gmail.com>
2024-07-12 09:35:34 +0800

    建立版面架構及載入功能頁面的原則

--- admin.php
+++ admin.php
@@ -27,18 +27,17 @@
       </marquee>
     </div>
     <div id="mm">
-      <div class="ct a rb" style="position:relative; width:101.5%; left:-1%; padding:3px; top:-9px;">
-        <a href="?do=tit">網站標題管理</a>|
-        <a href="?do=go">動態文字管理</a>|
-        <a href="?do=trailer">預告片海報管理</a>|
-        <a href="?do=movie">院線片管理</a>|
-        <a href="?do=order">電影訂票管理</a>
-      </div>
-      <div class="rb tab">
-        <h2 class="ct">請選擇所需功能</h2>
-      </div>
+      <?php
+      $do = $_GET['do'] ?? 'main';
+      $file = "backend/{$do}.php";
+      if (file_exists($file)) {
+        include $file;
+      } else {
+        include 'backend/main.php';
+      }
+      ?>
     </div>
-    <div id="bo"> ©Copyright 2010~2014 ABC影城 版權所有 </div>
+    <div id="bo"> ©Copyright 2020~2024 ABC影城 版權所有 </div>
   </div>
 </body>
 
