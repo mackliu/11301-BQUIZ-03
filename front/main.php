d48bdf430c7a29d22f2267ecba831fd1d1bc2d05
mackliu <macklun@gmail.com>
2024-07-15 13:37:21 +0800

    完成海報自動輸播

--- front/main.php
+++ front/main.php
@@ -7,7 +7,7 @@
                 $posters = $Poster->all(['sh' => 1], " Order By rank");
                 foreach ($posters as $poster) {
                 ?>
-                    <div class='poster'>
+                    <div class='poster' data-ani='<?= $poster['ani']; ?>'>
                         <img src="./images/<?= $poster['img']; ?>">
                         <div class='name'><?= $poster['name']; ?></div>
                     </div>
@@ -36,20 +36,46 @@
 
 <script>
     $(".poster").eq(0).show()
-    let start = 0;
+    let now = 0;
+    let next = 1;
     let slide = setInterval(() => {
         ani()
     }, 3000)
 
     function ani() {
-        $(".poster").eq(start).fadeOut(1000, () => {
-            if (start < $(".poster").length - 1) {
-                start++;
-            } else {
-                start = 0;
-            }
-            $(".poster").eq(start).fadeIn(1000)
-        })
+
+        let ani = parseInt($(".poster").eq(now).data('ani'))
+
+        if (now < $(".poster").length - 1) {
+            next = now + 1;
+        } else {
+            next = 0
+        }
+        //console.log(ani, now, next)
+        switch (ani) {
+            case 1:
+                //淡入淡出
+                $(".poster").eq(now).fadeOut(1000, () => {
+                    $(".poster").eq(next).fadeIn(1000)
+                })
+                break;
+            case 2:
+                //縮放
+                $(".poster").eq(now).hide(1000, () => {
+                    $(".poster").eq(next).show(1000)
+
+                })
+                break;
+            case 3:
+                //滑入滑出
+                $(".poster").eq(now).slideUp(1000, () => {
+                    $(".poster").eq(next).slideDown(1000)
+                })
+                break;
+        }
+
+        now = next
+
     }
 </script>
 
