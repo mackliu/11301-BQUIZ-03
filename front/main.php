9518c3adc806381ad1c39bd9284159119df87649
mackliu <macklun@gmail.com>
2024-07-15 14:55:13 +0800

    建立按鈕區水平移動機制

--- front/main.php
+++ front/main.php
@@ -83,6 +83,27 @@
         now = next
 
     }
+
+
+    $(".btn-left,.btn-right").on("click", function() {
+        console.log()
+        let direction = $(this).attr('class').split("-")[1]
+        switch (direction) {
+            case 'left':
+
+                $(".btn").animate({
+                    right: 80
+                });
+
+                break;
+            case 'right':
+
+                $(".btn").animate({
+                    right: 0
+                });
+                break;
+        }
+    })
 </script>
 
 
