a5fdb738ee0e66cba661b8f629d123cf2b0d5f83
mackliu <macklun@gmail.com>
2024-07-16 14:16:29 +0800

    完成院線片排序功能

--- backend/movie.php
+++ backend/movie.php
@@ -38,31 +38,33 @@
 <hr>
 <div style="width:100%;height:400px;overflow:auto;">
     <?php
-    $movies = $Movie->all(" order by rank");
-    foreach ($movies as $movie) {
+    $rows = $Movie->all(" order by rank");
+    foreach ($rows as $idx => $row) {
+        $prev = ($idx != 0) ? $rows[$idx - 1]['id'] : $row['id'];
+        $next = ($idx != (count($rows) - 1)) ? $rows[$idx + 1]['id'] : $row['id'];
     ?>
         <div class='movie'>
             <div class="img">
-                <img src="./images/<?= $movie['poster']; ?>" style="width:70px;height:90px">
+                <img src="./images/<?= $row['poster']; ?>" style="width:70px;height:90px">
             </div>
             <div class="level">
-                分級:<img src="./icon/03C0<?= $movie['level']; ?>.png" style="width:30px;">
+                分級:<img src="./icon/03C0<?= $row['level']; ?>.png" style="width:30px;">
             </div>
             <div class="info">
                 <div class="base">
-                    <div>片名：<?= $movie['name']; ?></div>
-                    <div>片長：<?= $movie['length']; ?>分鐘</div>
-                    <div>上映時間：<?= $movie['ondate']; ?></div>
+                    <div>片名：<?= $row['name']; ?></div>
+                    <div>片長：<?= $row['length']; ?>分鐘</div>
+                    <div>上映時間：<?= $row['ondate']; ?></div>
                 </div>
                 <div class="btns">
                     <button>顯示</button>
-                    <button>往上</button>
-                    <button>往下</button>
-                    <button onclick="location.href='?do=edit_movie&id=<?= $movie['id']; ?>'">編輯電影</button>
-                    <button onclick="del('Movie',<?= $movie['id']; ?>)">刪除電影</button>
+                    <button class='sw' data-sw='<?= $row['id']; ?>-<?= $prev; ?>'>往上</button>
+                    <button class='sw' data-sw='<?= $row['id']; ?>-<?= $next; ?>'>往下</button>
+                    <button onclick="location.href='?do=edit_movie&id=<?= $row['id']; ?>'">編輯電影</button>
+                    <button onclick="del('Movie',<?= $row['id']; ?>)">刪除電影</button>
                 </div>
                 <div class="intro">
-                    劇情介紹：<?= $movie['intro']; ?>
+                    劇情介紹：<?= $row['intro']; ?>
                 </div>
             </div>
         </div>
@@ -71,3 +73,16 @@
 
     ?>
 </div>
+
+<script>
+    $(".sw").on("click", function() {
+        //console.log($(this).data('sw'))
+        $.post("./api/sw.php", {
+            table: 'Movie',
+            sw: $(this).data('sw')
+        }, (res) => {
+            //console.log(res)
+            location.reload();
+        })
+    })
+</script>
